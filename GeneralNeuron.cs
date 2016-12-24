63179a61d916cb395edd98995c1438c35960831b
Zocker1999NET <zocker.net@live.de>
2016-12-24 12:52:42 +0100

    Change of first default class to interface "GeneralNeuron" GeneralNeuron represents a super class for all types of neurons. It only requires to have a function returning the current value of the neuron.

--- GeneralNeuron.cs
+++ GeneralNeuron.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace NeuralNetwork
-{
-    public class Class1
-    {
-    }
+namespace NeuralNetwork {
+	public interface GeneralNeuron {
+
+
+
+	}
 }
