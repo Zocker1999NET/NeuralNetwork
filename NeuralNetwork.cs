5f8f784c841cf02918781ccfbdcc28f4eafe1b14
Zocker1999NET <zocker.net@live.de>
2016-12-25 23:38:16 +0100

    Added Class NeuralNetwork NeuralNetworks can be paused which stops the recalculation of the controlled neurons even when some input values are changing.

---
+++ NeuralNetwork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork {
+	public abstract class NeuralNetwork {
+
+		private bool calcPaused = false;
+
+		public bool CalculationPaused {
+			get {
+				return calcPaused;
+			}
+			protected set {
+				calcPaused = value;
+			}
+		}
+
+	}
+}
