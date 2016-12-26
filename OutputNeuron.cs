6a4540b1d56ce9cf9402235d4cfc4b65cc00c8fa
Zocker1999NET <zocker.net@live.de>
2016-12-26 15:04:43 +0100

    OutputNeuron: Constructor and Inheritance changed Added basic definition of constructor Changed inheritance from DependentNeuron to HiddenNeuron

--- OutputNeuron.cs
+++ OutputNeuron.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 
 namespace NeuralNetwork {
-	public abstract class OutputNeuron : DependentNeuron {
+	public abstract class OutputNeuron : HiddenNeuron {
+
+		public OutputNeuron(NeuralNetwork network) : base(network) { }
 
 	}
 }
