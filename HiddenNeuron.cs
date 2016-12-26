3a57e794b028a66b4a526328519cd4845f588d2f
Zocker1999NET <zocker.net@live.de>
2016-12-26 15:28:50 +0100

    Added Function Functionality to HiddenNeuron Adapted constructor of OutputNeuron Automatically adapted class diagram NeuronTypes

--- HiddenNeuron.cs
+++ HiddenNeuron.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NeuralNetwork.Functions;
 
 namespace NeuralNetwork {
 	public class HiddenNeuron : DependentNeuron {
 
-		public HiddenNeuron(NeuralNetwork network) : base(network) { }
+		GeneralFunction actFunc;
+
+		public HiddenNeuron(NeuralNetwork network, GeneralFunction activationFunction) : base(network) {
+			actFunc = activationFunction;
+		}
+
+		public GeneralFunction ActivationFunction {
+			get {
+				return actFunc;
+			}
+		}
+
+		protected override float ActivationFunctionMethod(float value) {
+			return actFunc[value];
+		}
 
 	}
 }
--- OutputNeuron.cs
+++ OutputNeuron.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NeuralNetwork.Functions;
 
 namespace NeuralNetwork {
 	public abstract class OutputNeuron : HiddenNeuron {
 
-		public OutputNeuron(NeuralNetwork network) : base(network) { }
+		public OutputNeuron(NeuralNetwork network, GeneralFunction activationFunction) : base(network, activationFunction) { }
 
 	}
 }
