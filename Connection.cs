a250bddf89e37fa43e57e14615154c5e11f349ce
Zocker1999NET <zocker.net@live.de>
2016-12-26 11:31:07 +0100

    Excluded Class Connection in its own file Diagram NeuronTypes adapted automatically

---
+++ Connection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork {
+	public abstract partial class GeneralNeuron {
+		public class Connection {
+
+			GeneralNeuron output;
+			DependentNeuron input;
+			float weight = 0f;
+			bool change = false;
+
+			public Connection(GeneralNeuron outNeuron, DependentNeuron inNeuron, float weigth) {
+				output = outNeuron;
+				input = inNeuron;
+				output.outCon.Add(this);
+			}
+
+			public float Weigth {
+				get {
+					return weight;
+				}
+				set {
+					weight = value;
+				}
+			}
+
+			internal void FlagChange() {
+				change = true;
+				input.CalculateOutput();
+			}
+
+			public float WeightedInput {
+				get {
+					change = false;
+					return output.CurrentValue * weight;
+				}
+			}
+
+			public bool Change {
+				get {
+					return change;
+				}
+			}
+
+		}
+	}
+}
--- GeneralNeuron.cs
+++ GeneralNeuron.cs
@@ -5,49 +5,7 @@ using System.Text;
 using NeuralNetwork.Exceptions;
 
 namespace NeuralNetwork {
-	public abstract class GeneralNeuron {
-
-		public class Connection {
-
-			GeneralNeuron output;
-			DependentNeuron input;
-			float weight = 0f;
-			bool change = false;
-
-			public Connection(GeneralNeuron outNeuron, DependentNeuron inNeuron, float weigth) {
-				output = outNeuron;
-				input = inNeuron;
-				output.outCon.Add(this);
-			}
-
-			public float Weigth {
-				get {
-					return weight;
-				}
-				set {
-					weight = value;
-				}
-			}
-
-			public void FlagChange() {
-				change = true;
-				input.CalculateOutput();
-			}
-
-			public float WeightedInput {
-				get {
-					change = false;
-					return output.CurrentValue * weight;
-				}
-			}
-
-			public bool Change {
-				get {
-					return change;
-				}
-			}
-
-		}
+	public abstract partial class GeneralNeuron {
 
 		public static bool ValueOutOfRange(float value) {
 			return ( -1 > value || value > 1 );
@@ -66,7 +24,6 @@ namespace NeuralNetwork {
 		public GeneralNeuron(NeuralNetwork network) {
 			net = network;
 		}
-
 
 		public abstract float CalculateOutput();
 
