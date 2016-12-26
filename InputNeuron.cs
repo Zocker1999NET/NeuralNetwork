2ff82a5c11f9e99fb327e9a2a8801aa460ee2092
Zocker1999NET <zocker.net@live.de>
2016-12-26 11:31:57 +0100

    Added basic implemantion of Class InputNeuron

--- InputNeuron.cs
+++ InputNeuron.cs
@@ -7,7 +7,13 @@ using System.Threading.Tasks;
 namespace NeuralNetwork {
 	public abstract class InputNeuron : GeneralNeuron {
 
-		
+		public InputNeuron(NeuralNetwork network) : base(network) { }
+
+		public abstract float UpdateOutput();
+
+		public override float CalculateOutput() {
+			return UpdateOutput();
+		}
 
 	}
 }
