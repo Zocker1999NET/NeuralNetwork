174bcf6b4ed80ae6f0b11d15c6ea93393fcdf9a3
Zocker1999NET <zocker.net@live.de>
2016-12-30 13:58:39 +0100

    Added functionality to DelegateOutputNeuron
    Without commentary

--- DelegateOutputNeuron.cs
+++ DelegateOutputNeuron.cs
@@ -2,9 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NeuralNetwork.Functions;
 
 namespace NeuralNetwork {
 	public class DelegateOutputNeuron : OutputNeuron {
+
+		public delegate void OutputDelegate(float value);
+
+		private OutputDelegate outputMeth;
+
+		public DelegateOutputNeuron(NeuralNetwork network, GeneralFunction function) : base(network, function) { }
+
+		public DelegateOutputNeuron(NeuralNetwork network, GeneralFunction function, OutputDelegate outputMethod) : base(network, function) {
+			outputMeth = outputMethod;
+		}
+
+		public OutputDelegate OutputMethod {
+			get {
+				return outputMeth;
+			}
+			set {
+				outputMeth = value;
+			}
+		}
+
+		protected override void valueChanged(float val) {
+			outputMeth(val);
+		}
 
 	}
 }
