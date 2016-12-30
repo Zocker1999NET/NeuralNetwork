aa56d39c34b06dfcfa08d5648f252ee2ccc10bbb
Zocker1999NET <zocker.net@live.de>
2016-12-30 13:47:26 +0100

    NeuralNetwork: Added commentary

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -5,10 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NeuralNetwork {
+	/// <summary>
+	/// Represents a neural network with many neurons.
+	/// </summary>
 	public abstract class NeuralNetwork {
 
 		private bool calcPaused = false;
 
+		/// <summary>
+		/// Gets if the neural network is paused so the neurons inside will not recalculate their outputs on input changes.
+		/// </summary>
 		public bool CalculationPaused {
 			get {
 				return calcPaused;
