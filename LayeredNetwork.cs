38e66d0f29207565186ad3a48bbc302004758ec4
Zocker1999NET <zocker.net@live.de>
2017-01-04 14:01:32 +0100

    Build constructor of OneLayerNetwork
    Created some generic/general methods in LayeredNetwork to simplify the
    construction of layered networks.

--- LayeredNetwork.cs
+++ LayeredNetwork.cs
@@ -7,7 +7,24 @@ using System.Threading.Tasks;
 namespace NeuralNetwork {
 	public abstract class LayeredNetwork : NeuralNetwork {
 
+		protected delegate T genListDelegate<T>();
+		protected delegate T genListBoolDelegate<T>(bool config);
 
+		protected void addCountToList<T>(int count, genListDelegate<T> method, List<T> l) {
+			for(int c = 0; c < count; c++)
+				l.Add(method());
+		}
+
+		protected void addCountToList<T>(int count, genListBoolDelegate<T> method, List<T> l, bool config) {
+			for(int c = 0; c < count; c++)
+				l.Add(method(config));
+		}
+
+		protected void addConnections(DependentNeuron[] output, GeneralNeuron[] input) {
+			foreach(var o in output)
+				foreach(var i in input)
+					o.AddInputConnection(i, nextRandom());
+		}
 
 	}
 }
--- OneLayerNetwork.cs
+++ OneLayerNetwork.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NeuralNetwork.Functions;
 
 namespace NeuralNetwork {
 	/// <summary>
@@ -14,12 +15,31 @@ namespace NeuralNetwork {
 		private List<HiddenNeuron> hiddenLayer;
 		private List<OutputNeuron> outputLayer;
 
-		public OneLayerNetwork(int inputCount, int hiddenCount, int outputCount) {
-
+		/// <summary>
+		/// Generates a neural network with one layer.
+		/// </summary>
+		/// <param name="inputCount"></param>
+		/// <param name="hiddenCount"></param>
+		/// <param name="outputCount"></param>
+		/// <param name="boolOutput">If true, the output neurons will only return 0 or 1.</param>
+		public OneLayerNetwork(int inputCount, int hiddenCount, int outputCount, bool boolOutput) {
+			addCountToList(inputCount, generateInputNeuron, inputLayer);
+			addCountToList(hiddenCount, generateHiddenNeuron, hiddenLayer);
+			addCountToList(outputCount, generateOutputNeuron, outputLayer, boolOutput);
+			addConnections(hiddenLayer.ToArray(), inputLayer.ToArray());
+			addConnections(outputLayer.ToArray(), hiddenLayer.ToArray());
 		}
 
 		protected virtual InputNeuron generateInputNeuron() {
 			return new InputNeuron(this);
+		}
+
+		protected virtual HiddenNeuron generateHiddenNeuron() {
+			return new HiddenNeuron(this, new LogisticFunction(nextRandom(2)));
+		}
+
+		protected virtual OutputNeuron generateOutputNeuron(bool config) {
+			return config ? new OutputNeuron(this, null) : new OutputNeuron(this, new LogisticFunction(nextRandom(2)));
 		}
 
 	}
