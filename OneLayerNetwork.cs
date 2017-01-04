fd190810b4a81f4456b16b98ab0f7bbf995eb761
Zocker1999NET <zocker.net@live.de>
2017-01-04 13:10:32 +0100

    Started working on OneLayerNetwork

--- OneLayerNetwork.cs
+++ OneLayerNetwork.cs
@@ -5,9 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NeuralNetwork {
+	/// <summary>
+	/// Represents a layered neural network with one hidden layer.
+	/// </summary>
 	public class OneLayerNetwork : LayeredNetwork {
 
-			
+		private List<InputNeuron> inputLayer;
+		private List<HiddenNeuron> hiddenLayer;
+		private List<OutputNeuron> outputLayer;
+
+		public OneLayerNetwork(int inputCount, int hiddenCount, int outputCount) {
+
+		}
+
+		protected virtual InputNeuron generateInputNeuron() {
+			return new InputNeuron(this);
+		}
 
 	}
 }
