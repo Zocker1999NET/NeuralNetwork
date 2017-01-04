ec58202ecd3287fdc1b7ecd5614178d2d3399b92
Zocker1999NET <zocker.net@live.de>
2017-01-04 13:10:12 +0100

    Added LogisticFunction

---
+++ Functions/LogisticFunction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork.Functions {
+	public class LogisticFunction : GeneralFunction {
+
+		public LogisticFunction(double propConst) {
+			param = propConst;
+		}
+
+		public override double Function(double x) {
+			return ( 2 / ( 1 + ( Math.Pow(Math.E, ( -param ) * x) ) ) ) - 1;
+		}
+
+		public override double Inverse(double y) {
+			return -( Math.Log(( 2 / ( y + 1 ) ) - 1) / param );
+		}
+
+		public override bool SetParameterFor(double x, double y) {
+			param = -( Math.Log(( 2 / ( y + 1 ) ) - 1) / x );
+			return true;
+		}
+
+	}
+}
