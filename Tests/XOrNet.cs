147b0b5097cc1f8e62259cbb717bec94b2bf9452
Zocker1999NET <zocker.net@live.de>
2017-01-29 12:54:04 +0100

    Added Tests Namespace and XOrNet
    For testing the learning mechanism after it's implemented

---
+++ Tests/XOrNet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork.Tests {
+	public class XOrNet : OneLayerNetwork {
+
+		public XOrNet() : base(2,3,1,true) { }
+
+
+
+	}
+}
