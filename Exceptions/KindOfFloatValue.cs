a0b625114fffa366b361693605b5debf9fa06855
Felix Stupp <zocker.net@live.de>
2017-04-21 01:27:40 +0200

    Added Commentary to Exceptions.KindOfFloatValue

--- Exceptions/KindOfFloatValue.cs
+++ Exceptions/KindOfFloatValue.cs
@@ -5,8 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NeuralNetwork.Exceptions {
+	/// <summary>
+	/// Defines possible types of float values which can cause exceptions.
+	/// </summary>
 	public enum KindOfFloatValue {
+		/// <summary>
+		/// Unknown type of float value.
+		/// </summary>
 		Unknown,
+		/// <summary>
+		/// Float value for a connection weight.
+		/// </summary>
 		Weight,
 	}
 }
