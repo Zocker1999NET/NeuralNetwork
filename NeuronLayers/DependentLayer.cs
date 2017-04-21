1a01b8a205f9e8b1867db0b90902ab8d15523533
Felix Stupp <zocker.net@live.de>
2017-04-21 15:10:05 +0200

    Added NeuronLayers for LayeredNetworks Existing Types: GeneralLayer, DependentLayer, InputLayer, HiddenLayer, OutputLayer Also added a class diagram for NeuronLayers

---
+++ NeuronLayers/DependentLayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuralNetwork.Neurons;
+
+namespace NeuralNetwork.NeuronLayers {
+	public abstract class DependentLayer<T> : GeneralLayer<T> where T : DependentNeuron {
+		
+		
+		
+	}
+}
